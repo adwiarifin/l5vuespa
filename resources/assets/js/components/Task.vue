a8f7a4c93eb19d3ff918d247da84ff1ca451e5d8
Adwi Arifin <adwiarifin@gmail.com>
2018-03-10 08:02:23 +0700

    make vue component and compile it with mix

---
+++ resources/assets/js/components/Task.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col">
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">My Assignments</h5>
+            <p class="card-text">
+               
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    mounted() {
+
+    }
+  }
+</script>
